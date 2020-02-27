066eb4a13a9737c04c49bb04e9d75863a78176a3
guiwitz <guiwitz@gmail.com>
2020-02-28 00:38:26 +0100

    minor updates

--- installation/download_data.py
+++ installation/download_data.py
@@ -19,3 +19,9 @@ urllib.request.urlretrieve(url, where_to_save+'3colors.tif')
 
 url = 'https://cildata.crbs.ucsd.edu/media/images/13585/13585.tif' 
 urllib.request.urlretrieve(url, where_to_save+'myoblast.tif')
+
+url = 'https://cildata.crbs.ucsd.edu/media/images/13586/13586.tif' 
+urllib.request.urlretrieve(url, where_to_save+'myoblast2.tif')
+
+url = 'https://cildata.crbs.ucsd.edu/media/images/13590/13590.tif' 
+urllib.request.urlretrieve(url, where_to_save+'myoblast3.tif')
